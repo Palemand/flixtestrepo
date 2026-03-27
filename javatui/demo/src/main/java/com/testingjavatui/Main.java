522ff6a541fd4376f1639473b4bdff7913d84a42
Bertram <bertram.bib@gmail.com>
2026-03-27 12:55:40 +0100

    Retry Done showcase done

--- javatui/demo/src/main/java/com/testingjavatui/Main.java
+++ javatui/demo/src/main/java/com/testingjavatui/Main.java
@@ -2,11 +2,7 @@ package com.testingjavatui;
 
 public class Main {
     public static void main(String[] args) {
-        try {
-            RawModeTest.main(args);
-        } catch (Exception e) {
-            System.out.println(e.getStackTrace());
-        }
+        
     }
 }
 
